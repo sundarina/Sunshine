d4e8d52ebf7c554e7071b6168c3e7147ac02dd09
Sundarina <sundarina@ukr.net>
2016-06-01 21:34:39 +0300

    onCreateView() +Button onClick() +buttonIndex method for buttonIndex interfase for button selected

--- app/src/main/java/me/mihael/sundarina/sunshine/MainFragment.java
+++ app/src/main/java/me/mihael/sundarina/sunshine/MainFragment.java
@@ -1,12 +1,15 @@
 package me.mihael.sundarina.sunshine;
 
 import android.content.Context;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -18,7 +21,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 
-    public class MainFragment extends Fragment {
+    public class MainFragment extends Fragment implements View.OnClickListener {
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -46,16 +49,16 @@ import android.view.ViewGroup;
      */
     // TODO: Rename and change types and number of parameters
 
-   /** public static MainFragment newInstance(String param1, String param2) {
+   /* public static MainFragment newInstance(String param1, String param2) {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
+    }*/
 
-    **/
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -66,15 +69,80 @@ import android.view.ViewGroup;
         }
     }
 
+    //подключение разметки xml
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+            Bundle savedInstanceState) {
+
+
+       /*  container, false используются в связке и указывают на возможнось подключения фрагментов в активность через контейнер.
+       Мы обойдёмся без контейнеров, а создадим собственные блоки для фрагментов, поэтому у нас используется значение false.*/
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        //подключаем кнопки
+        /* метод findViewById() будет относиться уже не к классу Activity,
+        а к корневому элементу разметки фрагмента, в нашем случае rootView. */
+
+
+        Button button1 = (Button) rootView.findViewById(R.id.button1);
+        Button button2 = (Button) rootView.findViewById(R.id.button2);
+        Button button3 = (Button) rootView.findViewById(R.id.button3);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+
+        return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    /*public void onButtonPressed(Uri uri) {
+
+
+    @Override
+    public void onClick(View v) {
+        int buttonIndex = translateIdToIndex(v.getId());
+        OnSelectedButtonListener listener = (OnSelectedButtonListener) getActivity();
+        listener.onButtonSelected(buttonIndex);
+        //обработчик нажатий кнопок
+        // Временный код для получения индекса нажатой кнопки
+       // Toast.makeText(getActivity(), String.valueOf(buttonIndex), Toast.LENGTH_SHORT).show();
+    }
+
+    int translateIdToIndex(int id) {
+
+        /* код, который бы получал информацию о нажатой кнопке,
+        чтобы активность могла использовать эту информацию и использовать её для управления вторым фрагментом.
+        Создадим отдельный метод, который на основании идентификатора кнопки создаст нужный индекс:*/
+
+        int index = -1;
+        switch (id) {
+            case R.id.button1:
+                index = 1;
+                break;
+            case R.id.button2:
+                index = 2;
+                break;
+            case R.id.button3:
+                index = 3;
+                break;
+        }
+        return index;
+    }
+
+    public interface OnSelectedButtonListener {
+        void onButtonSelected(int buttonIndex);
+
+        /*
+        Информация о индексе нажатой кнопки доступна только самому фрагменту. Наша задача - передать эту информацию активности, которая затем передаст её другой активности.
+        Интерфейс не определяет работу метода, а только даёт ему имя.
+        Класс, который будет использовать данный интерфейс, должен придумать,
+        что делать в методе с данным именем.Иинтерфейс будет использовать класс активности.*/
+    }
+
+     /*   // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
         }
@@ -89,15 +157,28 @@ import android.view.ViewGroup;
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+    }*/
+
+
+    /*@Override
+    public void onResume() {
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR);
     }
 
     @Override
+    public void onPause() {
+        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // ваш код
+        super.onPause();
+    }*/
+
+   /* @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
+    }*/
 
-    /**
+    /*
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
@@ -111,8 +192,9 @@ import android.view.ViewGroup;
 
 
 
-    /*public interface OnFragmentInteractionListener {
+   /* public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }*/
+
 }
